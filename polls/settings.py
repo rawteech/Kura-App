929c3536472127eecf41a1a942d307fcba9fc747
rawteech <rotichbil636@gmail.com>
2017-02-09 18:57:35 +0300

    Register kura app in settings

--- polls/settings.py
+++ polls/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'kura.apps.KuraConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
