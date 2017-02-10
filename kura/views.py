7b5ca821d10cbf369bed0118dd5b35f488b827f2
rawteech <rotichbil636@gmail.com>
2017-02-10 13:29:24 +0300

    Added template functionality

--- kura/views.py
+++ kura/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from .models import Question
+
+
 def index(request):
-	return HttpResponse("Just testing out the views")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+	return HttpResponse("You are looking at question %s" % question_id)
+
+def results(request, question_id):
+	response = "You are looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You are voting on question %s." % question_id)
